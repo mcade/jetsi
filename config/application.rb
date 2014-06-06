04f314c7f5b236112b4ee4cc4c440fbe5d601e71
Maurice <mcadenhe@gmail.com>
2014-06-06 08:11:08 -0400

    Change image tags after failing to load assets for heroku

--- config/application.rb
+++ config/application.rb
@@ -27,10 +27,7 @@ module Jetsi
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-    config.serve_static_assets = true
-    config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
-    config.assets.compile = true
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.eot *.woff *.ttf)
     config.i18n.fallbacks = true
   end
 end
